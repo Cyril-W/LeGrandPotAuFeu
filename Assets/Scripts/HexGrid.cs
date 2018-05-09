b7c81d8702b4b1faea2690e690727db532459341
Cyril-W <cwirtz@ensc.fr>
2018-05-09 13:41:56 +0200

    Setting up the grid
    clickable to change the hexagone color

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum HexDirection
+{
+    NE, E, SE, SW, W, NW
+}
+
+public static class HexDirectionExtensions
+{
+
+    public static HexDirection Opposite(this HexDirection direction)
+    {
+        return (int)direction < 3 ? (direction + 3) : (direction - 3);
+    }
+}
+
+public class HexGrid : MonoBehaviour {
+
+    // Static part
+    public static float outerRadius = 10f;
+    public static float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius)
+    };
+
+    // Object part
+    [Header("Grid Size")]
+    public int width = 6;
+    public int height = 6;
+
+    [Header("Prefabs")]
+    public HexCell cellPrefab;
+    public Text cellLabelPrefab;
+
+    [Header("Cell Color")]
+    public Color defaultColor = Color.white;
+
+    HexCell[] cells;
+    Canvas gridCanvas;
+    HexMesh hexMesh;
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        HexCell cell = cells[i] = Instantiate(cellPrefab, transform);
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (outerRadius * 1.5f);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
+
+        if (x > 0)
+        {
+            cell.SetNeighbor(HexDirection.W, cells[i - 1]);
+        }
+        if (z > 0)
+        {
+            if ((z & 1) == 0)
+            {
+                cell.SetNeighbor(HexDirection.SE, cells[i - width]);
+                if (x > 0)
+                {
+                    cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
+                }
+            }
+            else
+            {
+                cell.SetNeighbor(HexDirection.SW, cells[i - width]);
+                if (x < width - 1)
+                {
+                    cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
+                }
+            }
+        }
+
+        Text label = Instantiate(cellLabelPrefab, gridCanvas.transform);
+        label.rectTransform.anchoredPosition = new Vector2(position.x - 1, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+        cell.gameObject.name = "Hex Cell " + cell.coordinates.ToString();
+    }
+
+    public void ColorCell(Vector3 position, Color color)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = color;
+        hexMesh.Triangulate(cells);
+    }
+}
