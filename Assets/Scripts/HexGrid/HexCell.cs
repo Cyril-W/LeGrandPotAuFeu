72d740c03a151fa9288a95872668ecc34cd7ace7
Cyril-W <cwirtz@ensc.fr>
2018-05-18 01:10:53 +0200

    Adding Water and refactoring HexMex/HexGridChunk

--- Assets/Scripts/HexGrid/HexCell.cs
+++ Assets/Scripts/HexGrid/HexCell.cs
@@ -1,103 +1,111 @@
 ﻿using UnityEngine;
 
-public class HexCell : MonoBehaviour {
+namespace LeGrandPotAuFeu.HexGrid {
+	public class HexCell : MonoBehaviour {
+		public HexCoordinates coordinates;
+		public RectTransform uiRect;
+		public HexGridChunk chunk;
+		public int WaterLevel {
+			get {
+				return waterLevel;
+			}
+			set {
+				if (waterLevel == value) {
+					return;
+				}
+				waterLevel = value;
+				Refresh();
+			}
+		}
+		int waterLevel;
+		public bool IsUnderwater {
+			get {
+				return waterLevel > elevation;
+			}
+		}
+		public float WaterSurfaceY {
+			get {
+				return
+					(waterLevel + HexMetrics.waterElevationOffset) *
+					HexMetrics.elevationStep;
+			}
+		}
+		public Color Color {
+			get {
+				return color;
+			}
+			set {
+				if (color == value) {
+					return;
+				}
+				color = value;
+				Refresh();
+			}
+		}
+		Color color;
+		public Vector3 Position {
+			get {
+				return transform.localPosition;
+			}
+		}
+		public int Elevation {
+			get {
+				return elevation;
+			}
+			set {
+				if (elevation == value) {
+					return;
+				}
 
-    public HexCoordinates coordinates;
-    public RectTransform uiRect;
-    public HexGridChunk chunk;
-    public Color Color
-    {
-        get
-        {
-            return color;
-        }
-        set
-        {
-            if (color == value)
-            {
-                return;
-            }
-            color = value;
-            Refresh();
-        }
-    }
-    Color color;
-    public Vector3 Position
-    {
-        get
-        {
-            return transform.localPosition;
-        }
-    }
-    public int Elevation
-    {
-        get
-        {
-            return elevation;
-        }
-        set
-        {
-            if (elevation == value)
-            {
-                return;
-            }
+				elevation = value;
+				Vector3 position = transform.localPosition;
+				position.y = value * HexMetrics.elevationStep;
+				position.y += (HexMetrics.SampleNoise(position).y * 2f - 1f) * HexMetrics.elevationPerturbStrength;
+				transform.localPosition = position;
 
-            elevation = value;
-            Vector3 position = transform.localPosition;
-            position.y = value * HexMetrics.elevationStep;
-            position.y += (HexMetrics.SampleNoise(position).y * 2f - 1f) * HexMetrics.elevationPerturbStrength;
-            transform.localPosition = position;
+				Vector3 uiPosition = uiRect.localPosition;
+				uiPosition.z = -position.y;
+				uiRect.localPosition = uiPosition;
 
-            Vector3 uiPosition = uiRect.localPosition;
-            uiPosition.z = -position.y;
-            uiRect.localPosition = uiPosition;
+				Refresh();
+			}
+		}
+		int elevation = int.MinValue;
 
-            Refresh();
-        }
-    }
-    int elevation = int.MinValue;
+		[SerializeField]
+		HexCell[] neighbors;
 
-    [SerializeField]
-    HexCell[] neighbors;
+		public HexCell GetNeighbor(HexDirection direction) {
+			return neighbors[(int)direction];
+		}
 
-    public HexCell GetNeighbor(HexDirection direction)
-    {
-        return neighbors[(int)direction];
-    }
+		public void SetNeighbor(HexDirection direction, HexCell cell) {
+			neighbors[(int)direction] = cell;
+			cell.neighbors[(int)direction.Opposite()] = this;
+		}
 
-    public void SetNeighbor(HexDirection direction, HexCell cell)
-    {
-        neighbors[(int)direction] = cell;
-        cell.neighbors[(int)direction.Opposite()] = this;
-    }
+		public HexEdgeType GetEdgeType(HexDirection direction) {
+			return HexMetrics.GetEdgeType(
+					elevation, neighbors[(int)direction].elevation
+			);
+		}
 
-    public HexEdgeType GetEdgeType(HexDirection direction)
-    {
-        return HexMetrics.GetEdgeType(
-            elevation, neighbors[(int)direction].elevation
-        );
-    }
+		public HexEdgeType GetEdgeType(HexCell otherCell) {
+			return HexMetrics.GetEdgeType(
+					elevation, otherCell.elevation
+			);
+		}
 
-    public HexEdgeType GetEdgeType(HexCell otherCell)
-    {
-        return HexMetrics.GetEdgeType(
-            elevation, otherCell.elevation
-        );
-    }
-
-    void Refresh()
-    {
-        if (chunk)
-        {
-            chunk.Refresh();
-            for (int i = 0; i < neighbors.Length; i++)
-            {
-                HexCell neighbor = neighbors[i];
-                if (neighbor != null && neighbor.chunk != chunk)
-                {
-                    neighbor.chunk.Refresh();
-                }
-            }
-        }
-    }
+		void Refresh() {
+			if (chunk) {
+				chunk.Refresh();
+				for (int i = 0; i < neighbors.Length; i++) {
+					HexCell neighbor = neighbors[i];
+					if (neighbor != null && neighbor.chunk != chunk) {
+						neighbor.chunk.Refresh();
+					}
+				}
+			}
+		}
+	}
 }
--- Assets/Scripts/HexGrid/HexMapEditor.cs
+++ Assets/Scripts/HexGrid/HexMapEditor.cs
@@ -1,106 +1,100 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HexMapEditor : MonoBehaviour
-{
-    [Header("Color to pick from the UI")]
-    public Color[] colors;
+namespace LeGrandPotAuFeu.HexGrid {
+	public class HexMapEditor : MonoBehaviour {
+		[Header("Color to pick from the UI")]
+		public Color[] colors;
 
-    [Header("Drag'n'Drop")]
-    public HexGrid hexGrid;
+		[Header("Drag'n'Drop")]
+		public HexGrid hexGrid;
 
-    Color activeColor;
-    int activeElevation;
-    bool applyColor;
-    bool applyElevation = true;
-    int brushSize;
+		Color activeColor;
+		int activeElevation;
+		int activeWaterLevel;
+		bool applyColor;
+		bool applyElevation = true;
+		bool applyWaterLevel = true;
+		int brushSize;
 
-    void Awake()
-    {
-        SelectColor(-1);
-        SetElevation(0);
-    }
+		void Awake() {
+			SelectColor(-1);
+			SetElevation(0);
+		}
 
-    void Update()
-    {
-        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
-        {
-            HandleInput();
-        }
-    }
+		void Update() {
+			if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) {
+				HandleInput();
+			}
+		}
 
-    void HandleInput()
-    {
-        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(inputRay, out hit))
-        {
-            EditCells(hexGrid.GetCell(hit.point));
-        }
-    }
+		void HandleInput() {
+			Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(inputRay, out hit)) {
+				EditCells(hexGrid.GetCell(hit.point));
+			}
+		}
 
-    void EditCells(HexCell center)
-    {
-        int centerX = center.coordinates.X;
-        int centerZ = center.coordinates.Z;
+		void EditCells(HexCell center) {
+			int centerX = center.coordinates.X;
+			int centerZ = center.coordinates.Z;
 
-        for (int r = 0, z = centerZ - brushSize; z <= centerZ; z++, r++)
-        {
-            for (int x = centerX - r; x <= centerX + brushSize; x++)
-            {
-                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
-            }
-        }
-        for (int r = 0, z = centerZ + brushSize; z > centerZ; z--, r++)
-        {
-            for (int x = centerX - brushSize; x <= centerX + r; x++)
-            {
-                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
-            }
-        }
-    }
+			for (int r = 0, z = centerZ - brushSize; z <= centerZ; z++, r++) {
+				for (int x = centerX - r; x <= centerX + brushSize; x++) {
+					EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+				}
+			}
+			for (int r = 0, z = centerZ + brushSize; z > centerZ; z--, r++) {
+				for (int x = centerX - brushSize; x <= centerX + r; x++) {
+					EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+				}
+			}
+		}
 
-    void EditCell(HexCell cell)
-    {
-        if (cell)
-        {
-            if (applyColor)
-            {
-                cell.Color = activeColor;
-            }
-            if (applyElevation)
-            {
-                cell.Elevation = activeElevation;
-            }
-        }
-    }
+		void EditCell(HexCell cell) {
+			if (cell) {
+				if (applyColor) {
+					cell.Color = activeColor;
+				}
+				if (applyElevation) {
+					cell.Elevation = activeElevation;
+				}
+				if (applyWaterLevel) {
+					cell.WaterLevel = activeWaterLevel;
+				}
+			}
+		}
 
-    public void SelectColor(int index)
-    {
-        applyColor = index >= 0;
-        if (applyColor)
-        {
-            activeColor = colors[index];
-        }
-    }
+		public void SelectColor(int index) {
+			applyColor = index >= 0;
+			if (applyColor) {
+				activeColor = colors[index];
+			}
+		}
 
-    public void SetElevation(float elevation)
-    {
-        activeElevation = (int)elevation;
-    }
+		public void SetElevation(float elevation) {
+			activeElevation = (int)elevation;
+		}
 
-    public void SetApplyElevation(bool toggle)
-    {
-        applyElevation = toggle;
-    }
+		public void SetApplyElevation(bool toggle) {
+			applyElevation = toggle;
+		}
 
-    public void SetBrushSize(float size)
-    {
-        brushSize = (int)size;
-    }
+		public void SetApplyWaterLevel(bool toggle) {
+			applyWaterLevel = toggle;
+		}
 
-    public void ShowUI(bool visible)
-    {
-        hexGrid.ShowUI(visible);
-    }
+		public void SetWaterLevel(float level) {
+			activeWaterLevel = (int)level;
+		}
+
+		public void SetBrushSize(float size) {
+			brushSize = (int)size;
+		}
+
+		public void ShowUI(bool visible) {
+			hexGrid.ShowUI(visible);
+		}
+	}
 }
